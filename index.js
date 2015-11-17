8ad76b47d6e490884a06b39d77ec8536d5a0c43f
saphron <saphron.scul@gmail.com>
2015-11-17 11:28:08 -0500

    lab exercise to import module

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// this is how we import modules
-// for modules registered with npm, we merely give a name
-// for our own files, we give a relative path
+// to import modules registered with npm, give a name
+// to import our own files, give relative path
+
 var ourModule = require('./module');
 
-// your module is available here as `ourModule`
-// use it!
-
+ourModule.name = "Coriander";
+ourModule.greeting();
